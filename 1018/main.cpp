6d5e76b97baf0b5588f58fa20d64ecdcfe9b0195
k123s456h <k123s456h@gmail.com>
2020-04-06 00:49:37 +0000

    8^8

--- 1018/main.cpp
+++ 1018/main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 
+using namespace std;
+
 int main(void){
+    int width=0, height=0, i=0, j=0;
+    char** board;
+
+    cin >> height >> width;
+    board = new char*[height];
+
+    for(i=0; i<height; i++){
+        board[i] = new char[width];
+        for(j=0; j<width; j++){
+            cin >> board[i][j];
+        }
+    }
+
+    
+
+    for(i=0; i<height; i++) delete[] board[i];
+    delete[] board;
+
     return 0;
 }
