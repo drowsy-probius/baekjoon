7aef2b6ebeb0d9df57459877170526b7c6a3fdbb
k123s456h <k123s456h@gmail.com>
2020-04-07 04:30:58 +0000

    8^8

---
+++ 2108/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#define MAX 4000
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(false);
+    vector<int> arr;
+    int size=0;
+
+    cin >> size;
+    arr.resize(size);
+
+    
+
+    return 0;
+}
