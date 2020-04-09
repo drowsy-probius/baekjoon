dc0d741f5b41c5999489ec5500e150c13f8dbb2a
k123s456h <k123s456h@gmail.com>
2020-04-09 02:55:21 +0000

    8^8

--- 1427/main.cpp
+++ 1427/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
+
 int main(void){
     ios::sync_with_stdio(false);
-    char arr[10] = {-1, };
+    char arr[11] = {-1, };
     cin >> arr;
 
-    for(int i=0; i<10 && arr[i]!= -1; i++){
-        
+    sort(arr, arr+11, [](char a, char b){return a>b;});
+
+    for(int i=0; i<11; i++){
+        if(arr[i] != -1) cout << arr[i];
     }
 
     return 0;
